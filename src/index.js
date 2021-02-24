b1303e7b84b105e3e0142120616ee688a966225f
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-02-24 20:57:27 +0200

    Add dotenv

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const app = require("./app");
 
-app.listen(8081, () => {
-	console.log(`Server is listening at http://localhost:8081`);
+const port = process.env.PORT ?? 8081;
+
+console.log("NODE_ENV:", app.get("env"));
+
+app.listen(port, () => {
+	console.log(`Server is listening at http://localhost:${port}`);
 });
